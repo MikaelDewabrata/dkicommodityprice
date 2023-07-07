ce84f5ee748695b9bc1e63ae47421c1ce9a1570f
monsterikan <57279779+monsterikan@users.noreply.github.com>
2023-07-07 11:46:48 +0700

    Create dags_pasar.py

---
+++ airflow/dags_pasar.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  5 16:35:46 2023
+
+@author: mikaeld
+"""
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+from pythonfile.run.get_latest_API import getjson
+
+default_args = {
+    'owner': 'mikael',  
+    'start_date': datetime(2023, 6, 26),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=10),
+}
+
+dag = DAG(
+    'run_market_price',
+    default_args=default_args,
+    schedule_interval='0 6 * * *',  # Run daily at 1 PM (13:00)
+)
+
+with dag:
+    get_json = PythonOperator(task_id='getjson',
+                              python_callable=getjson
+                              )
