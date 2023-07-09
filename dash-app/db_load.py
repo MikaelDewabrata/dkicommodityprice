c02459307050d5d4cc0a3aa64937a0818896e543
monsterikan <57279779+monsterikan@users.noreply.github.com>
2023-07-09 12:07:49 +0700

    Add files via upload

---
+++ dash-app/db_load.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  8 20:59:51 2023
+
+@author: MIKAEL
+"""
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+def db_load_pasardki():
+    # Step 2: Create SQLAlchemy engine
+    engine = create_engine("mysql+mysqlconnector://{user}:{pw}@{host}/{db}"
+                           .format(user="mikael",
+                                   pw="Peipei8!",
+                                   host="38.47.180.145",
+                                   db="pasardkidb"))
+
+    # Step 3: Establish database connection
+    with engine.connect() as connection:
+        query = """
+            SELECT ml.MarketName, cl.Commodity, al.Date, al.Price
+            FROM marketlist AS ml
+            JOIN allpasar AS al ON al.MarketID = ml.MarketID
+            JOIN commoditylist AS cl ON al.CommodityID = cl.CommodityID
+        """
+
+        # Step 4: Execute the query and fetch the result into a pandas DataFrame
+        df = pd.read_sql_query(query, connection)
+
+    return df
+
